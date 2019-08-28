d968d0cf6bb4eb1f900e796e5c71fcf861eb3cb6
Sophie THEIS <sophiets13@gmail.com>
2019-08-28 17:28:02 +0200

    update apoptosis function...

--- polarity/apoptosis.py
+++ polarity/apoptosis.py
@@ -1,28 +1,25 @@
-import random
 import numpy as np
-
 from tyssue.utils.decorators import face_lookup
 from tyssue.geometry.sheet_geometry import SheetGeometry
-
-from tyssue.behaviors.sheet.actions import contract, ab_pull, exchange, remove, contract
-from tyssue.behaviors.sheet.basic_events import contraction
+from tyssue.behaviors.sheet.actions import (ab_pull,
+                                            exchange,
+                                            remove,
+                                            decrease,
+                                            increase_linear_tension)
+from tyssue.behaviors.sheet.basic_events import contraction_line_tension
 
 
 def apoptosis_patterning(sheet, manager, **kwargs):
-    """Apoptosis process patterning, which put cell in apoptosis progressively.
-
-
-    """
-
-    # variable a passer en parametre?
+    # ViscousSolver...
+    # Need to be imported from a notebook...
 
     return 0
-
 
 
 default_apoptosis_spec = {
     "face_id": -1,
     "face": -1,
+    "shrink_rate": 1.1,
     "critical_area": 1e-2,
     "radial_tension": 0.1,
     "contract_rate": 0.1,
@@ -68,15 +65,24 @@ def apoptosis(sheet, manager, **kwargs):
     current_traction = apoptosis_spec["current_traction"]
     face_area = sheet.face_df.loc[face, "area"]
 
-    if face_area > apoptosis_spec["critical_area"]:
-        # contract
-        contract(
+    if (face_area > apoptosis_spec["critical_area"]):
+        # reduce prefered_area
+        decrease(sheet,
+                 'face',
+                 face,
+                 apoptosis_spec["shrink_rate"],
+                 col="prefered_area",
+                 divide=True,
+                 bound=apoptosis_spec["critical_area"] / 2,
+                 )
+
+        increase_linear_tension(
             sheet,
             face,
-            apoptosis_spec["contract_rate"] * dt,
-            False)
-
-        ## Rajouter une condition sur la constriction des voisins ou non ?
+            apoptosis_spec['contract_rate'] * dt,
+            multiple=True,
+            isotropic=True,
+            limit=100)
 
         # contract neighbors
         neighbors = sheet.get_neighborhood(
@@ -86,17 +92,17 @@ def apoptosis(sheet, manager, **kwargs):
         manager.extend(
             [
                 (
-                    contraction,
+                    contraction_line_tension,
                     _neighbor_contractile_increase(
                         neighbor, dt, apoptosis_spec),
                 )
                 for _, neighbor in neighbors.iterrows()
             ])
 
-    if face_area < 20 * apoptosis_spec["critical_area"]:
+    if face_area < apoptosis_spec["critical_area"]:
         if current_traction < apoptosis_spec["max_traction"]:
             ab_pull(sheet, face, apoptosis_spec[
-                "radial_tension"], distributed=False)
+                "radial_tension"], distributed=True)
             current_traction = current_traction + dt
             apoptosis_spec.update({"current_traction": current_traction})
 
@@ -121,11 +127,41 @@ def _neighbor_contractile_increase(neighbor, dt, apoptosis_spec):
 
     specs = {
         "face_id": neighbor["id"],
-        "contractile_increase": increase,
-        "critical_area": apoptosis_spec["critical_area"] * dt,
+        "contractile_increase": increase * dt,
+        "critical_area": apoptosis_spec["critical_area"],
         "max_contractility": 50,
         "multiple": True,
         "unique": False,
     }
 
     return specs
+
+
+def propagate_line_tension(sheet, face):
+    neighbors = sheet.get_neighborhood(face, 8).dropna()
+
+    for _, neighbor in neighbors.iterrows():
+        print(neighbor.face, neighbor.order)
+        V1 = (sheet.face_df.loc[face][sheet.coords] -
+              sheet.face_df.loc[neighbor][sheet.coords])
+        # Axe proximo-distale -> à choisir en le passant en paramètre ?
+        V2 = [0, 0, 1]
+        dot_product = np.dot(V1, V2)
+        d_V1 = np.sqrt(V1.x**2 + V1.y**2 + V1.z**2)
+        d_V2 = np.sqrt(V2[0]**2 + V2[1]**2 + V2[2]**2)
+
+        angle = np.arccosh(dot_product / (d_V1 * d_V2))
+
+        if (angle >= (np.pi / 3)) and (angle <= (2 * np.pi / 3)):
+
+            # augmenter "beaucoup" la tension linéaire sur 7/8 voisins
+            # ou augmenter la tension linéaire que pour les jonctions parralèle
+            # au pli ?
+            pass
+        else:
+            # augmenter "légèrement" la tension linéaire sur 2/3 voisins
+            # ou augmenter la tension linéaire que pour les jonctions
+            # perpendiculaire au pli ?
+            pass
+
+    return
