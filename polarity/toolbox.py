bda022e2ac85939f3e3c3074ae64d073d8502744
Sophie THEIS <sophiets13@gmail.com>
2020-07-02 11:34:41 +0200

    Remove unused notebook

--- polarity/toolbox.py
+++ polarity/toolbox.py
@@ -8,10 +8,6 @@ from tyssue import Sheet
 from tyssue.io import hdf5
 from tyssue import config
 
-from ..settings import (apopto_pattern_kwargs,
-                        apoptosis_settings,
-                        contraction_lt_kwargs,
-                        delaminate_settings)
 
 
 def init_tissue(hf5_filename='superegg_final.hf5',
@@ -249,3 +245,49 @@ def all_depth_calculation(directory, zmin_=-7, zmax_=7):
             pass
 
     return depths
+
+
+from polarity.polarity import model
+from polarity.dynamics import EllipsoidLameGeometry as geom
+
+apopto_pattern_kwargs = {'t': 0.,
+                         'dt': 1.,
+                         'time_of_last_apoptosis': 30.}
+# Apoptosis
+apoptosis_settings = {
+    "critical_area_pulling": 10,
+    "critical_area": 0.5,
+    "contract_rate": 1.08,
+    "basal_contract_rate": 1.01,
+    "contract_neighbors": True,
+    "contract_span": 3,
+    "radial_tension": 50.,
+    "max_traction": 30.,
+    "current_traction": 0.,
+    "geom": geom,
+}
+
+# Neighbors propagation
+contraction_lt_kwargs = {
+    'face_id': -1,
+    'face': -1,
+    'shrink_rate': 1.05,
+    'critical_area': 5.,
+    "contraction_column": "line_tension",
+    "model": model,
+}
+
+
+# Mechanical perturbation
+delaminate_settings = {
+    'radial_tension': 80,
+    "contract_rate": 1.08,
+    "critical_area_pulling": 15,
+    "critical_area": 1e-2,
+    'current_traction': 0,
+    'max_traction': 150,
+    'contract_neighbors': True,
+    'contract_span': 3,
+    'basal_contract_rate': 1.01,
+    'geom': geom,
+    'model': model}
